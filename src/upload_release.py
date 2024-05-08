c182aa86f359ccdf7387372a8c78c2914601092e
tosox <57193602+Tosox@users.noreply.github.com>
2024-05-08 13:09:55 +0200

    feat: update script

---
+++ src/upload_release.py
@@ -0,0 +1,21 @@
+import os
+import moddb
+
+def set_output(name, value):
+    with open(os.getenv("GITHUB_OUTPUT"), 'a') as f:
+        print(f"{name}={value}", file=f)
+
+def main():
+    username = os.getenv("MODDB_USERNAME")
+    password = os.getenv("MODDB_PASSWORD")
+
+    try:
+        moddb.login(username, password)
+    except ValueError as e:
+        print(e)
+        exit(-1)
+
+    set_output(f"Successfully logged-in as {username}")
+
+if __name__ == '__main__':
+    main()
